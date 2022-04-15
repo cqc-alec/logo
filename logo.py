1b8488a7c0d9e4a683d4fbf4dc0e381fec810f6a
Alec Edgington <alec.edgington@cambridgequantum.com>
2022-04-15 10:13:30 +0100

    Comments.

--- logo.py
+++ logo.py
@@ -6,9 +6,10 @@ WIDTH, HEIGHT = 12 * W, 8 * W
 surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, WIDTH, HEIGHT)
 ctx = cairo.Context(surface)
 
-ctx.set_source_rgb(1, 1, 1)
+ctx.set_source_rgb(1, 1, 1)  # white background
 ctx.paint()
 
+# Draw T..T
 ctx.move_to(W, W)
 ctx.line_to(11 * W, W)
 ctx.line_to(11 * W, 2 * W)
@@ -22,9 +23,10 @@ ctx.line_to(3 * W, 7 * W)
 ctx.line_to(3 * W, 2 * W)
 ctx.line_to(W, 2 * W)
 ctx.line_to(W, W)
-ctx.set_source_rgb(0.75, 0, 0)
+ctx.set_source_rgb(0.75, 0, 0)  # red
 ctx.fill()
 
+# Draw .KE.
 ctx.move_to(4 * W, 2 * W)
 ctx.line_to(5 * W, 2 * W)
 ctx.line_to(5 * W, 4 * W)
@@ -44,7 +46,7 @@ ctx.line_to(5 * W, 5 * W)
 ctx.line_to(5 * W, 7 * W)
 ctx.line_to(4 * W, 7 * W)
 ctx.line_to(4 * W, 2 * W)
-ctx.set_source_rgb(0, 0.75, 0)
+ctx.set_source_rgb(0, 0.75, 0)  # green
 ctx.fill()
 
 surface.write_to_png("logo.png")
